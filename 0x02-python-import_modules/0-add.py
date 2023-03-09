da27a38e693c09fabdbbf102229f1a39b019cd08
Ezinne <ekalu.fk@gmail.com>
2023-03-09 17:50:04 +0100

    fixing task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
-import add_0 as addy
+#!/usr/bin/python3
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    """Print some of 1 and 2"""
+    from add_0 import add
 
-print("{} + {} = {}".format(a, b, addy.add(a, b)))
-
-
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
