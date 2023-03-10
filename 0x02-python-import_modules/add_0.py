0a2f5de5ce29d659cf2aed0b85fe432e6163528e
Ezinne <ekalu.fk@gmail.com>
2023-03-10 15:38:10 +0100

    fixed task

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,3 +1,12 @@
-def add(a,b):
-    result = a + b
-    return result
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
