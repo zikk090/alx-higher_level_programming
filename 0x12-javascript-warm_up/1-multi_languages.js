a4f8ad18b9a0744bae53d4dc231daf33dfe39d03
Ezinne <ekalu.fk@gmail.com>
2023-06-13 15:33:56 +0100

    checking

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
 /*
 THIS WAS WRONG. I NEED JUST ONE console.log line
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
 */
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
