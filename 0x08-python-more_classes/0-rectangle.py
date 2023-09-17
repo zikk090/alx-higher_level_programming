1c0967a88f860cc619359e41b5f3b05f7114c99d
Ezinne <ekalu.fk@gmail.com>
2023-09-17 10:07:51 +0100

    this is a commit message

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This code defines an empty class.
+"""
+
+
+class Rectangle:
+    """Represents an empty rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is a late submission for my project. By Ezinne Kalu
